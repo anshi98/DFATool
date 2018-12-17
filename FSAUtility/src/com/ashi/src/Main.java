abc4fe17488b3ff0b8da8871ceead26634a92598
anshi98 <andyshi700@gmail.com>
2018-12-16 22:55:46 -0500

    Added comments to main.

--- FSAUtility/src/com/ashi/src/Main.java
+++ FSAUtility/src/com/ashi/src/Main.java
@@ -26,10 +26,11 @@ import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Main {
-	public static final int HEIGHT = 1000;
-	public static final int WIDTH = 1000;
+	public static final int HEIGHT = 1000; // Height of main JFrame
+	public static final int WIDTH = 1000; // Width of main JFrame
+	public static final int QUERY_STRING_INPUT_LENGTH = 80; // The length of the query string input window
 	public static GUI gui = new GUI();
-	public static JFrame f = new JFrame("Automata Simulator");
+	public static JFrame mainFrame = new JFrame("Automata Simulator");
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -42,35 +43,46 @@ public class Main {
 
 	/**
 	 * Initializes all GUI elements of the application (Menu bar and main
-	 * interactive interface (Testing interface and node interface))
+	 * interactive interface (Bar to enter query string with test button and GUI to
+	 * create and manipulate nodes))
 	 */
 	public static void initUI() {
 
-		f.setSize(WIDTH, HEIGHT);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setVisible(true);
-		f.setLayout(new BorderLayout());
-		f.add(gui, BorderLayout.CENTER);
+		mainFrame.setSize(WIDTH, HEIGHT);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setVisible(true);
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.add(gui, BorderLayout.CENTER);
 
 		initMenus();
 		initInputInterface();
 	}
 
 	/**
-	 * Initializes the input options interface for testing the automata
+	 * Initializes the interface for entering query strings and testing them with
+	 * the automata
 	 */
 	private static void initInputInterface() {
 		JPanel interfacePanel = new JPanel();
 		interfacePanel.setLayout(new BorderLayout());
 
-		JTextField inputsBox = new JTextField(80);
+		JTextField inputsBox = new JTextField(QUERY_STRING_INPUT_LENGTH);
 
 		interfacePanel.add(inputsBox, BorderLayout.WEST);
 		interfacePanel.add(createTestInputButton(inputsBox), BorderLayout.CENTER);
 
-		f.add(interfacePanel, BorderLayout.NORTH);
+		mainFrame.add(interfacePanel, BorderLayout.NORTH);
 	}
 
+	/**
+	 * Creates a "test" button for the interface to test automata with a query
+	 * string. Button works by seeing if query string is empty. If not, it'll look
+	 * for a node with its state as "beginning". If it finds one, it'll create a new
+	 * testing window
+	 * 
+	 * @param The box for inputing a query string
+	 * @return The testing button
+	 */
 	private static Component createTestInputButton(JTextField inputsBox) {
 		JButton toReturn = new JButton("Test");
 		toReturn.addActionListener(e -> {
@@ -78,47 +90,68 @@ public class Main {
 
 				for (Node node : gui.getNodes()) {
 					if (node.getNodeState() == NodeState.BEGINNING) {
-						List<String> tokens = new ArrayList<>(Arrays.asList(inputsBox.getText().split(" ")));
-
-						JFrame testFrame = new JFrame("Test input");
-						testFrame.setSize(TestInputView.WIDTH, TestInputView.HEIGHT);
-
-						testFrame.add(new TestInputView(tokens, node));
-						testFrame.setVisible(true);
-
-						testFrame.addWindowListener(new WindowAdapter() {
-							@Override
-							public void windowClosing(WindowEvent e) {
-								f.setEnabled(true);
-								gui.setTestNode(null);
-							}
-
-						});
-
-						f.setEnabled(false);
-
-						return;
+						createTestingInterface(node, inputsBox);
 					}
 				}
 
 				JOptionPane.showMessageDialog(null, "ERROR: No beginning node");
+			} else {
+				JOptionPane.showMessageDialog(null, "ERROR: Empty query string");
 			}
 		});
 		return toReturn;
 	}
 
+	/**
+	 * Creates a testing window with the selected node and tokens from the query
+	 * string input box. The code will also temporarily disable the main window
+	 * until testing finishes (The testing window closes)
+	 * 
+	 * @param The       node to test
+	 * @param inputsBox
+	 */
+	private static void createTestingInterface(Node node, JTextField inputsBox) {
+		List<String> tokens = new ArrayList<>(Arrays.asList(inputsBox.getText().split(" ")));
+
+		JFrame testFrame = new JFrame("Test input");
+		testFrame.setSize(TestInputView.WIDTH, TestInputView.HEIGHT);
+
+		testFrame.add(new TestInputView(tokens, node));
+		testFrame.setVisible(true);
+
+		testFrame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				mainFrame.setEnabled(true);
+				gui.setTestNode(null);
+			}
+
+		});
+
+		mainFrame.setEnabled(false);
+
+		return;
+	}
+
+	/**
+	 * Creates a menu bar for the main frame (Contains file menu and edit menu)
+	 */
 	private static void initMenus() {
 		JMenuBar menuBar = new JMenuBar();
 
 		menuBar.add(createFileMenu());
 
-		//
-
 		menuBar.add(createEditMenu());
 
-		f.setJMenuBar(menuBar);
+		mainFrame.setJMenuBar(menuBar);
 	}
 
+	/**
+	 * Creates the edit menu for the menu bar (Contains option for editing node
+	 * connections and nodes themselves)
+	 * 
+	 * @return The edit menu
+	 */
 	private static JMenu createEditMenu() {
 		JMenu editMenu = new JMenu("Edit");
 
@@ -139,6 +172,12 @@ public class Main {
 		return editMenu;
 	}
 
+	/**
+	 * Creates a button for turning the selected node (If there is one) into a
+	 * regular node. Also refreshes the GUI
+	 * 
+	 * @return Button for turning node into regular node
+	 */
 	private static JMenuItem createRegularNodeButton() {
 		JMenuItem toReturn = new JMenuItem("Regular Node");
 		toReturn.addActionListener(e -> {
@@ -152,6 +191,12 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates a button for turning the selected node (If there is one) into a
+	 * terminal node. Also refreshes the GUI
+	 * 
+	 * @return Button for turning node into terminal node
+	 */
 	private static JMenuItem createTerminalNodeButton() {
 		JMenuItem toReturn = new JMenuItem("Terminal Node");
 		toReturn.addActionListener(e -> {
@@ -166,6 +211,12 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates a button for turning the selected node (If there is one) into a
+	 * beginning node (Assuming there isn't one already). Also refreshes the GUI
+	 * 
+	 * @return Button for turning node into beginning node
+	 */
 	private static JMenuItem createBeginningNodeButton() {
 		JMenuItem toReturn = new JMenuItem("Beginning Node");
 		toReturn.addActionListener(e -> {
@@ -192,6 +243,11 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates button for editing the name of a node
+	 * 
+	 * @return The button for editing a node name
+	 */
 	private static JMenuItem createEditNodeButton() {
 		JMenuItem toReturn = new JMenuItem("Edit node");
 		toReturn.addActionListener(e -> {
@@ -221,24 +277,16 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates a button that creates a window to edit the selected node connection
+	 * 
+	 * @return Button to create connection editing window
+	 */
 	private static JMenuItem createEditConnectionButton() {
 		JMenuItem toReturn = new JMenuItem("Edit connection");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedConnection() != null) {
-				JFrame frame = new JFrame("Edit Connection");
-				frame.setSize(EditConnectionView.WIDTH, EditConnectionView.HEIGHT);
-				frame.add(new EditConnectionView(gui.getSelectedConnection()));
-				frame.setVisible(true);
-
-				frame.addWindowListener(new WindowAdapter() {
-					@Override
-					public void windowClosing(WindowEvent e) {
-						f.setEnabled(true);
-					}
-
-				});
-
-				f.setEnabled(false);
+				createEditConnectionView();
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR: No connection selected");
 			}
@@ -246,6 +294,32 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates a window to connection editing view
+	 */
+	private static void createEditConnectionView() {
+		JFrame frame = new JFrame("Edit Connection");
+		frame.setSize(EditConnectionView.WIDTH, EditConnectionView.HEIGHT);
+		frame.add(new EditConnectionView(gui.getSelectedConnection()));
+		frame.setVisible(true);
+
+		frame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				mainFrame.setEnabled(true);
+			}
+
+		});
+
+		mainFrame.setEnabled(false);
+	}
+
+	/**
+	 * Creates the file menu for the menu bar. Contains buttons for writing out and
+	 * reading in program state, and exiting the program
+	 * 
+	 * @return The file menu
+	 */
 	private static JMenu createFileMenu() {
 		JMenu fileMenu = new JMenu("File");
 		fileMenu.add(createWriteOutButton());
@@ -255,6 +329,11 @@ public class Main {
 		return fileMenu;
 	}
 
+	/**
+	 * Creates button for closing program
+	 * 
+	 * @return Exit button
+	 */
 	private static JMenuItem createExitItemButton() {
 		JMenuItem toReturn = new JMenuItem("Exit");
 		toReturn.addActionListener(e -> {
@@ -263,6 +342,12 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates button to read in program serial. Prompts user for file and then
+	 * replaced old GUI object with read in GUI
+	 * 
+	 * @return Read in button
+	 */
 	private static JMenuItem createReadInButton() {
 		JMenuItem toReturn = new JMenuItem("Read in state");
 		toReturn.addActionListener(e -> {
@@ -283,7 +368,6 @@ public class Main {
 
 					gui.repaint();
 
-					System.out.println(gui.getNodes());
 					in.close();
 					fileIn.close();
 
@@ -295,6 +379,10 @@ public class Main {
 		return toReturn;
 	}
 
+	/**
+	 * Creates button to write out GUI state in "serials" folder
+	 * @return
+	 */
 	private static JMenuItem createWriteOutButton() {
 		JMenuItem toReturn = new JMenuItem("Write out state");
 		toReturn.addActionListener(e -> {
