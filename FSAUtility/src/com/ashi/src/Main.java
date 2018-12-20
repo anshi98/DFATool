7e22247f111d6bfba0fcc007b1d43a489bff3b69
anshi98 <andyshi700@gmail.com>
2018-12-19 19:43:21 -0500

    Finalized Main.

--- FSAUtility/src/com/ashi/src/Main.java
+++ FSAUtility/src/com/ashi/src/Main.java
@@ -4,12 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -30,8 +32,8 @@ public class Main {
 	public static final int HEIGHT = 1000; // Height of main JFrame
 	public static final int WIDTH = 1000; // Width of main JFrame
 	public static final int QUERY_STRING_INPUT_LENGTH = 80; // The length of the query string input window
-	public static GUI gui = new GUI();
-	public static JFrame mainFrame = new JFrame("Automata Simulator");
+	public static GUI gui = new GUI(); // The JPanel object to manipulate nodes with
+	public static JFrame mainFrame = new JFrame("Automata Simulator"); // The main window
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
@@ -55,6 +57,7 @@ public class Main {
 		mainFrame.setLayout(new BorderLayout());
 		mainFrame.add(gui, BorderLayout.CENTER);
 
+		// Request focus for GUI so keyListener works
 		gui.requestFocus();
 
 		initMenus();
@@ -90,7 +93,9 @@ public class Main {
 		JButton toReturn = new JButton("Test");
 		toReturn.addActionListener(e -> {
 			if (!inputsBox.getText().equals("")) {
+				// Query string not empty
 
+				// Check for beginning node to start testing on
 				for (Node node : gui.getNodes()) {
 					if (node.getNodeState() == NodeState.BEGINNING) {
 						createTestingInterface(node, inputsBox);
@@ -98,6 +103,7 @@ public class Main {
 					}
 				}
 
+				// No beginning loop if you can exit out of the loop without returning
 				JOptionPane.showMessageDialog(null, "ERROR: No beginning node");
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR: Empty query string");
@@ -111,19 +117,24 @@ public class Main {
 	 * string input box. The code will also temporarily disable the main window
 	 * until testing finishes (The testing window closes)
 	 * 
-	 * @param The       node to test
-	 * @param inputsBox
+	 * @param node      The node to test
+	 * @param inputsBox The window containing the query string
 	 */
 	private static void createTestingInterface(Node node, JTextField inputsBox) {
+		// Split the query string into segments
 		List<String> tokens = new ArrayList<>(Arrays.asList(inputsBox.getText().split(" ")));
 
 		JFrame testFrame = new JFrame("Test input");
 		testFrame.setSize(TestInputView.WIDTH,
 				TestInputView.BASE_HEIGHT + TestInputView.HEIGHT_PER_CONNECTION_ITEM * tokens.size());
 
+		// Add a new TestInputView JPanel with the string segment and the node with
+		// state BEGINNING to start testing
 		testFrame.add(new TestInputView(tokens, node));
 		testFrame.setVisible(true);
 
+		// Make it so that the main frame will be reenabled upon the testing window
+		// closing
 		testFrame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
@@ -133,6 +144,7 @@ public class Main {
 
 		});
 
+		// Disable the main window
 		mainFrame.setEnabled(false);
 
 		return;
@@ -143,11 +155,8 @@ public class Main {
 	 */
 	private static void initMenus() {
 		JMenuBar menuBar = new JMenuBar();
-
 		menuBar.add(createFileMenu());
-
 		menuBar.add(createEditMenu());
-
 		mainFrame.setJMenuBar(menuBar);
 	}
 
@@ -164,7 +173,7 @@ public class Main {
 		editMenu.addSeparator();
 		editMenu.add(createEditNodeButton());
 
-		//
+		/* ---SEPARATOR--- */
 
 		JMenu stateMenu = new JMenu("Change State");
 
@@ -187,7 +196,10 @@ public class Main {
 		JMenuItem toReturn = new JMenuItem("Regular Node");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedNode() != null) {
+				// User has selected a node to change
 				gui.getSelectedNode().setNodeState(NodeState.REGULAR);
+
+				// Refresh the screen
 				gui.repaint();
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR: No node selected");
@@ -206,7 +218,10 @@ public class Main {
 		JMenuItem toReturn = new JMenuItem("Terminal Node");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedNode() != null) {
+				// User has selected a node to change
 				gui.getSelectedNode().setNodeState(NodeState.TERMINAL);
+
+				// Refresh the screen
 				gui.repaint();
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR: No node selected");
@@ -226,17 +241,23 @@ public class Main {
 		JMenuItem toReturn = new JMenuItem("Beginning Node");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedNode() != null) {
+				// User has selected a node to change
 				boolean alreadyHasBeginningNode = false;
 
 				for (Node currNode : gui.getNodes()) {
 					if (currNode.getNodeState().equals(NodeState.BEGINNING)) {
+						// GUI already has one beginning node, so don't add any more by setting this
+						// boolean, so the next block won't be called
 						alreadyHasBeginningNode = true;
 						break;
 					}
 				}
 
 				if (!alreadyHasBeginningNode) {
+					// Assign the BEGINNING state to the node
 					gui.getSelectedNode().setNodeState(NodeState.BEGINNING);
+
+					// Refresh the screen
 					gui.repaint();
 				} else {
 					JOptionPane.showMessageDialog(null, "ERROR: Beginning node already exists");
@@ -257,6 +278,7 @@ public class Main {
 		JMenuItem toReturn = new JMenuItem("Edit node");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedNode() != null) {
+				// User has selected a node to change
 				String newName;
 
 				if ((newName = JOptionPane.showInputDialog(null, "Enter new node name")) != null) {
@@ -264,14 +286,18 @@ public class Main {
 
 					for (Node node : gui.getNodes()) {
 						if (node.getName().equals(newName)) {
+							// Name has already been assigned to another node, so set this boolean to
+							// prevent the next block from being called
 							nameTaken = true;
 							break;
 						}
 					}
 
 					if (!nameTaken) {
+						// Set new name
 						gui.getSelectedNode().setName(newName);
 
+						// Refresh the screen
 						gui.repaint();
 					} else {
 						JOptionPane.showMessageDialog(null, "ERROR: Name already taken");
@@ -291,6 +317,7 @@ public class Main {
 		JMenuItem toReturn = new JMenuItem("Edit connection");
 		toReturn.addActionListener(e -> {
 			if (gui.getSelectedConnection() != null) {
+				// User has connection a node to change
 				createEditConnectionView();
 			} else {
 				JOptionPane.showMessageDialog(null, "ERROR: No connection selected");
@@ -305,14 +332,18 @@ public class Main {
 	private static void createEditConnectionView() {
 		JFrame frame = new JFrame("Edit Connection");
 		frame.setSize(EditConnectionView.WIDTH, EditConnectionView.HEIGHT);
+		// Pass the currently selected connection to the new window
 		frame.add(new EditConnectionView(gui.getSelectedConnection()));
 		frame.setVisible(true);
 
+		// This adds two functionalities that are called when the user finishes editing
+		// the connection. It will check to see whether the user removed all keywords
+		// from the connection, and will delete the connection if so. Upon closing the
+		// window, the main frame will also be re-enabled
 		frame.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent e) {
 				// Check if connection has no more triggers
-
 				Connection selectedConnection = gui.getSelectedConnection();
 				if (selectedConnection.getTriggers().isEmpty()) {
 					// No more accepted keywords, so remove the connection
@@ -324,6 +355,7 @@ public class Main {
 
 		});
 
+		// Temporarily disable the main frame
 		mainFrame.setEnabled(false);
 	}
 
@@ -357,17 +389,24 @@ public class Main {
 
 	/**
 	 * Creates button to read in program serial. Prompts user for file and then
-	 * replaced old GUI object with read in GUI
+	 * replaces old GUI object with new read-in GUI object
 	 * 
 	 * @return Read in button
 	 */
 	private static JMenuItem createReadInButton() {
 		JMenuItem toReturn = new JMenuItem("Read in state");
 		toReturn.addActionListener(e -> {
+			// Create a new file chooser object
 			JFileChooser chooser = new JFileChooser("Select State File");
+
+			// Set default directory to serials folder, so the user doesn't have to
+			// painstakingly access it manually
 			chooser.setCurrentDirectory(new File("./serials/"));
+
+			// Make it so that only .ser files are selectable
 			FileNameExtensionFilter filter = new FileNameExtensionFilter("SER Files", "ser");
 			chooser.setFileFilter(filter);
+
 			chooser.showOpenDialog(null);
 			if (chooser.getSelectedFile() != null) {
 				try {
@@ -375,6 +414,7 @@ public class Main {
 					ObjectInputStream in = new ObjectInputStream(fileIn);
 
 					GUI newGui = (GUI) in.readObject();
+					// Replace variables of old GUI with new GUI
 					gui.setNodes(newGui.getNodes());
 					gui.setSelectedNode(newGui.getSelectedNode());
 					gui.setBeginningNode(newGui.getBeginningNode());
@@ -406,11 +446,14 @@ public class Main {
 				String serialName = JOptionPane.showInputDialog(null, "Enter name of file");
 
 				if (serialName != null) {
+					// User entered file name (Didn't click cancel)
 					FileOutputStream fileOut = new FileOutputStream("./serials/" + serialName + ".ser");
 					ObjectOutputStream out = new ObjectOutputStream(fileOut);
 					out.writeObject(gui);
 					out.close();
 					fileOut.close();
+
+					// Tell the user the file has been written out successfully
 					System.out.printf("Serialized data is saved in ./serials/" + serialName + ".ser");
 				}
 			} catch (IOException i) {
