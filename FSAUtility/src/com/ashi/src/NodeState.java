c239f7738c4199c5c6f47c4dd336ba72f3a2d44f
anshi98 <andyshi700@gmail.com>
2018-12-19 19:09:22 -0500

    Finalized NodeState

--- FSAUtility/src/com/ashi/src/NodeState.java
+++ FSAUtility/src/com/ashi/src/NodeState.java
@@ -3,8 +3,8 @@ package com.ashi.src;
 /**
  * Enumeration object used to assign node state. Node can be a regular node with
  * no special properties, a beginning node to which the automata starts testing
- * from and a terminal node which, upon reaching, means that the query string is
- * successful in this automata
+ * from and a terminal node. A query string is successful if, by the end of the
+ * query, a terminal node is reached
  * 
  * @author Andy
  *
